7e06e729e4f1828fe9962ed7c6e2ea1a4365bf5e
unsevillano08 <158488778+unsevillano08@users.noreply.github.com>
2026-04-08 19:10:07 +0200

    Generación de codigo

---
+++ DDF/uah_mars_drone_freertos_hito1/asw/components/ccdronemng/include/public/ccdronemng_iface_v1.h
@@ -0,0 +1,10 @@
+#ifndef CCDRONEMNG_H_
+#define CCDRONEMNG_H_
+
+//******************************************************************************
+// EDROOM Service Library
+
+#define _EDROOM_IS_EMBEDDED_
+
+#include <public/edroomsl_iface_v1.h>
+
