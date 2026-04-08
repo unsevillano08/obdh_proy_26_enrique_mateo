4d65a5883e9e5a357dd85032feffb587ad605f9d
unsevillano08 <158488778+unsevillano08@users.noreply.github.com>
2026-04-08 19:28:32 +0200

    Errores corregidos

--- DDF/EDROOMModel/UAHMarsDrone/ClasesActor/CCDroneMngC.cpp
+++ DDF/EDROOMModel/UAHMarsDrone/ClasesActor/CCDroneMngC.cpp
@@ -17,7 +17,7 @@ CCDroneMng::EDROOM_CTX_Top_0::EDROOM_CTX_Top_0(CCDroneMng &act ):
 	MsgBack(EDROOMcomponent.MsgBack),
 	DroneMngCtrl(EDROOMcomponent.DroneMngCtrl),
 	Timer(EDROOMcomponent.Timer),
-	VNextCtrl(1)
+	VNextCtrl(0.1)
 {
 }
 
@@ -28,7 +28,7 @@ CCDroneMng::EDROOM_CTX_Top_0::EDROOM_CTX_Top_0(EDROOM_CTX_Top_0 &context):
 	MsgBack(context.MsgBack),
 	DroneMngCtrl(context.DroneMngCtrl),
 	Timer(context.Timer),
-	VNextCtrl(1)
+	VNextCtrl(0.1)
 {
 
 }
@@ -115,7 +115,8 @@ void	CCDroneMng::EDROOM_CTX_Top_0::FInitFlightPlan()
 	//Timing Service useful methods
 	 
 	time.GetTime(); // Get current monotonic time
-	time.Add(0,VNextCtrl); // Add X sec + Y microsec
+	time = time + VNextCtrl;
+	//time.Add(0,VNextCtrl); // Add X sec + Y microsec
 	 
  
  
@@ -137,7 +138,8 @@ void	CCDroneMng::EDROOM_CTX_Top_0::FProgNextCtrl()
 	//Timing Service useful methods
 	 
 	time.GetTime(); // Get current monotonic time
-	time.Add(0,VNextCtrl); // Add X sec + Y microsec
+	time = time + VNextCtrl;
+	//time.Add(0,VNextCtrl); // Add X sec + Y microsec
 	 
    //Program absolute timer 
    Timer.InformAt( time ); 
